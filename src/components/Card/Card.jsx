17abbdff00eb56527f32265bf0351991ddaf9f7d
Arthur Osipov <osartie@gmail.com>
2022-06-03 22:30:08 +0200

    Создал первые компоненты

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,23 @@
+function Card(props) {
+  return (
+    <div className="card">
+      <div className="favorite">
+        <img src="/img/unliked.svg" alt="Unliked" />
+      </div>
+      <img width={133} height={112} src="/img/sneakers/1.jpg" alt="Sneakers" />
+      <h5>Мужские Кроссовки Nuke Blazer Mid Suede</h5>
+      <div className="d-flex justify-between align-center">
+        <div className="d-flex flex-column">
+          <span>Цена:</span>
+          <b>{props.price} руб.</b>
+        </div>
+        <button className="button">
+          <img width={11} height={11} src="/img/plus.svg" alt="Plus" />
+        </button>
+      </div>
+    </div>
+  )
+
+}
+
+export { Card };
